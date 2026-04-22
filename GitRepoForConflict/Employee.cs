560b3bda9fb25297f4942b016d6685b629f786d6
yuvrajgadadare <yuvraj.gadadare@gmail.com>
2026-04-22 08:05:34 +0530

    add Employee class from Tester Branch

---
+++ GitRepoForConflict/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitRepoForConflict
+{
+    internal class Employee
+    {
+        public void GetEmployees()
+        {
+            Console.WriteLine("Hi Hello");
+        }
+    }
+}
